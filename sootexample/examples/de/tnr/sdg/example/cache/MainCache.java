3096ee2e9234b3509275f379a0a199f5db7ca31e
Invice <froog.32@gmail.com>
2017-07-03 19:31:01 +0200

    match query

--- sootexample/examples/de/tnr/sdg/example/cache/MainCache.java
+++ sootexample/examples/de/tnr/sdg/example/cache/MainCache.java
@@ -3,13 +3,20 @@ package de.tnr.sdg.example.cache;
 public class MainCache {
 
 	private Cache cache;
+	private Cache cache2 = new Cache();
 	
 	public MainCache(){
 	}
 	
+	/*
+	 * Test if operator != is recognized
+	 */
 	public void createCache(){
 		//System.out.println("Initialising Cache.");
-		this.cache = new Cache();
+		if (cache == null) {
+			this.cache = new Cache();
+		} else {
+		}
 	}
 	
 	public void clearCache(){
@@ -20,21 +27,33 @@ public class MainCache {
 		}
 	}
 	
-//	public void cacheFile(String filename){
-//		//System.out.println("Calling cacheFile("+filename+").");
-//		if (cache != null) {
-//			cache.put(filename);
-//		} else {
-//		}
-//	}
-//	
+	public void cacheFile(String filename){
+		//System.out.println("Calling cacheFile("+filename+").");
+		if (cache != null) {
+			cache.put(filename);
+		} else {
+		}
+	}
+	
+	/*
+	 * Test if operand must be null
+	 */
+	public boolean checkDifference() {
+		if (cache==cache2) {
+			cache.reset();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) {
 		MainCache myCache = new MainCache();
 		
-//		myCache.cacheFile("I'm a File!");
+		myCache.cacheFile("I'm a File!");
 		myCache.clearCache();
 		myCache.createCache();
-//		myCache.cacheFile("Cooler File");
+		myCache.cacheFile("Cooler File");
 		myCache.clearCache();
 	}
 }
